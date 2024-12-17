d82156da36d8684989b1a4170e7f9b959eb65cc4
katethequeen <cateresa4@gmail.com>
2024-12-17 16:48:49 +0100

    Initial commit

---
+++ praticaS5L2/src/main/java/it/epicode/praticaS5L2/PraticaS5L2Application.java
@@ -0,0 +1,13 @@
+package it.epicode.praticaS5L2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaS5L2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaS5L2Application.class, args);
+	}
+
+}
---
+++ praticaS5L2/src/test/java/it/epicode/praticaS5L2/PraticaS5L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.praticaS5L2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaS5L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
